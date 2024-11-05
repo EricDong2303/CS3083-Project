c39d8ff93ab2d5114e6c73df30c128b7d8e8c044
Eric Dong <108444931+EricDong2303@users.noreply.github.com>
2024-11-05 13:39:39 -0500

    Update tables.sql with customer table

--- database/tables.sql
+++ database/tables.sql
@@ -1,1 +1,18 @@
-
+CREATE TABLE `database_project`.`customer` (
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `first_name` VARCHAR(100) NOT NULL,
+  `last_name` VARCHAR(100) NOT NULL,
+  `building_number` INT NOT NULL,
+  `street_name` VARCHAR(100) NOT NULL,
+  `apt_number` VARCHAR(100) NOT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `state` VARCHAR(100) NOT NULL,
+  `zipcode` INT NOT NULL,
+  `passport_number` INT NOT NULL,
+  `passport_exp` DATE NOT NULL,
+  `passport_country` VARCHAR(100) NOT NULL,
+  `dob` DATE NOT NULL,
+  `customercol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`email`));
+);
