9fdeb4545d822a32aa1f9f30b66d263dbc83a06b
Eric Dong <108444931+EricDong2303@users.noreply.github.com>
2024-11-05 16:10:37 -0500

    Update tables.sql

--- database/tables.sql
+++ database/tables.sql
@@ -5,28 +5,28 @@ CREATE TABLE `database_project`.`customer` (
   last_name VARCHAR(100) NOT NULL,
   building_number INT NOT NULL,
   street_name VARCHAR(100) NOT NULL,
-  `apt_number VARCHAR(100) NOT NULL,
+  apt_number VARCHAR(100) NOT NULL,
   city VARCHAR(100) NOT NULL,
   state VARCHAR(100) NOT NULL,
   zipcode INT NOT NULL,
   passport_number INT NOT NULL,
   passport_exp DATE NOT NULL,
   passport_country VARCHAR(100) NOT NULL,
-  dob` DATE NOT NULL,
-  PRIMARY KEY (`email`)
+  dob DATE NOT NULL,
+  PRIMARY KEY (email)
 );
 
 CREATE TABLE `database_project`.`customer_phone` (
 	email VARCHAR(100) NOT NULL,
 	phone_number INT NOT NULL,
-	PRIMARY KEY (`email`, `phone_number`)
+	PRIMARY KEY (email, phone_number)
 	foreign key(email) references customer(email)
 );
 
 CREATE TABLE `database_project`.`purchases`(
     email varchar(100) not null,
     ticket_id varchar(100) not null,
-    primary key(`email`, `ticket_id`),
+    primary key(email, ticket_id),
     foreign key(email) references customer(email),
     foreign key(ticket_id) references ticket(ticket_id)
 );
