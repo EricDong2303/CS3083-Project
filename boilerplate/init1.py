45cc3ffa5cf0518d01ac7cac4980890051a5b7d3
pearmeow <perryhuang2015@gmail.com>
2024-11-19 20:04:46 -0500

    changed db in init

--- boilerplate/init1.py
+++ boilerplate/init1.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
-                       db='blog',
+                       db='air_ticket_reservation_system',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
