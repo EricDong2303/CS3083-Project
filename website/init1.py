6403e12eccd13c7e2b0bd0db74d6fbc79a213ef7
pearmeow <perryhuang2015@gmail.com>
2024-12-05 21:50:26 -0500

    percent s

--- website/init1.py
+++ website/init1.py
@@ -138,7 +138,7 @@ def registerAuthCustomer():
     else:
         # Insert the new user into the customer table
         try:
-            insert_query = '''INSERT INTO customer VALUES (%s, md5(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            insert_query = '''INSERT INTO customer VALUES (%s, md5(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
             cursor.execute(insert_query, (email, password, first_name, last_name,
                                       building_number, street_name,
                                       apartment_number, city, state, zipcode,
